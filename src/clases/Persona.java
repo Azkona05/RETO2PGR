5c2a08d70fa155064a505dce6baa082e6a438120
Azkona05 <135417707+Azkona05@users.noreply.github.com>
2026-01-27 09:59:23 +0100

    clases
    clases creadas

---
+++ src/clases/Persona.java
@@ -0,0 +1,13 @@
+package clases;
+
+import java.io.Serializable;
+
+public abstract class Persona implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+}
---
+++ src/exception/ValidarOrigenVuelo.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class ValidarOrigenVuelo extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ValidarOrigenVuelo(String msg) {
+		super(msg);
+	}
+
+}
